860a06075f1a7de3baa3ed5f9706e4e1ddf63eea
jbousquet <jean-aurel.bousquet@docavenue.com>
2021-10-26 14:33:14 +0200

    Initial commit

---
+++ redux/reducer.js
@@ -0,0 +1,7 @@
+const initialState = {};
+
+const myReducer = (state = initialState, action) => {
+    // TODO
+}
+
+export default myReducer;
---
+++ styles/colors.js
@@ -0,0 +1,5 @@
+export const colors = {
+    primary: '#64C9C3',
+    secondary: '#F68092',
+    text: '#606060'
+}
